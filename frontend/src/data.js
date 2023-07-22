554dd04f35fc1313dde87d6f8e315ea2491ea976
smitex1 <smithpraise61@gmail.com>
2023-07-22 15:24:19 +0100

    message

---
+++ frontend/src/data.js
@@ -0,0 +1,75 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/3195979/pexels-photo-3195979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      title: "ANNIVERSARY SALE",
+      desc: "DON'T COMPROMISE ON QUALIITY! GET 50% OFF ON ALL ITEMS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/16497412/pexels-photo-16497412/free-photo-of-woman-dressed-in-black-gown-posing-in-park-at-sunset.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      title: "EVENING GOWNS",
+      desc: "DON'T COMPROMISE ON QUALITY! GET 50% OFF FOR ALL ITEMS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://img.freepik.com/premium-photo/portrait-businesswoman-outside-office_255667-14841.jpg?w=900",
+      title: "FORMAL WEAR",
+      desc: "DON'T COMPROMISE ON QUALITY! GET 50% OFF FOR ALL ITEMS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/16497412/pexels-photo-16497412/free-photo-of-woman-dressed-in-black-gown-posing-in-park-at-sunset.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      title: "EVENING COSTUME ",
+    },
+    {
+      id: 2,
+      img: "https://img.freepik.com/premium-photo/portrait-businesswoman-outside-office_255667-14841.jpg?w=900",
+      title: "FORMAL WEAR",
+    },
+    {
+      id: 3,
+      img: "https://img.freepik.com/free-photo/beautiful-tanned-fit-caucasian-woman-bronze-shiny-skin-bikini-with-coconut-oil-by-blue-swimming-pool-sunny-day_343596-2355.jpg?w=900&t=st=1689988636~exp=1689989236~hmac=7d8032e51f75de3ad33e2f52c2f0068bd364f4c771278e31e9b8f0f4ae7cbca7",
+      title: "BIKINI CHARM",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://img.freepik.com/free-psd/female-t-shirt-mockup_23-292935581.jpg?w=740&t=st=1689989284~exp=1689989884~hmac=ab31fbc699b9b6375fd2b8446ed3a01a629df0c8f9ca64cd866e9244f4738dae",
+    },
+    {
+      id:2,
+      img:"https://images.pexels.com/photos/3768005/pexels-photo-3768005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://images.pexels.com/photos/1232459/pexels-photo-1232459.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id:5,
+      img:"https://images.pexels.com/photos/2849742/pexels-photo-2849742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id:6,
+      img:"https://images.pexels.com/photos/6624862/pexels-photo-6624862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",   },
+    {
+      id:7,
+      img:"https://images.pexels.com/photos/3782789/pexels-photo-3782789.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id:8,
+      img:"https://images.pexels.com/photos/904350/pexels-photo-904350.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+]
